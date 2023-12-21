600991a3198ed80f08604ca6b8cda84c03025284
JDSVGFedora <jdsvg997@outlook.com>
2023-12-21 09:05:59 -0500

    Ajustes

---
+++ 4_Arrays.cpp
@@ -0,0 +1,8 @@
+#include <iostream>	 //llamando la libreria
+using namespace std; //asignando namespace std al proyecto → /* https://openwebinars.net/blog/que-son-los-namespaces-en-c/ */
+//
+int main() //Inicializador del programa
+{
+/*   */
+
+}//main
