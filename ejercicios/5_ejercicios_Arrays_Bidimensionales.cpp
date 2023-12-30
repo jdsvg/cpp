a402472ae006f9cdb53a7221b426b12df6ff0bbb
JDSVGFedora <jdsvg997@outlook.com>
2023-12-30 16:12:34 -0500

    Ejercicios de matrices

--- ejercicios/5_ejercicios_Arrays_Bidimensionales.cpp
+++ ejercicios/5_ejercicios_Arrays_Bidimensionales.cpp
@@ -71,20 +71,24 @@ int main()      // Inicializador del programa
         c   d           g   h       (c*e)+(d*g)     (c*f)+(g*g)
 
     */
-    int aux = 0, aux2 = 0, aux3 = 0, aux4 = 0;
+    // int aux = 0, aux2 = 0, aux3 = 0, aux4 = 0;
     // cout << "Indique cuantas filas y columnas tendran sus matrices: ";cin >> aux;cin.ignore(256, '\n');
     // int A[aux][aux], B[aux][aux];
 
-    aux = 3;
-    int A[aux][aux] = { {1,2,3},
-                        {4,5,6},
-                        {7,8,9},};
-                        
-    int B[aux][aux] = { {5,6,7},
-                        {2,1,3},
-                        {2,3,1},};
-    int C[aux][aux];
 
+    // Ejemplo rapido
+    // aux = 3;
+    // int A[aux][aux] = { {1,2,3},
+    //                     {4,5,6},
+    //                     {7,8,9},};
+                        
+    // int B[aux][aux] = { {5,6,7},
+    //                     {2,1,3},
+    //                     {2,3,1},};
+    // int C[aux][aux];
+
+
+    // Proceso normal
     // cout << endl;
     // cout << endl;
     // for (int x = 0; x < aux; x++)
@@ -101,22 +105,81 @@ int main()      // Inicializador del programa
     //     {cout << "Ingrese el valor para la matriz B, en la posicion [" << x << "][" << y << "]: ";cin >> B[x][y];cin.ignore(256, '\n');}cout << endl;
     // }
 
+    // cout << endl;
+    // cout << endl;
+    // for (int x = 0; x < aux; x++)
+    // {
+    //     for (int y = 0; y < aux; y++)
+    //     {
+    //         C[x][y]=0;
+    //         for (int i = 0; i < aux; i++)
+    //         {
+    //             C[x][y]=C[x][y]+     (A[x][i]*B[i][y]);
+    //         }
+    //         cout<<C[x][y]<<" ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+    /*
+        EJERCICIO 3
+
+        Escriba un programa que genere la matriz transpuesta de una matriz dada.
+
+        A = {   1,2,3        
+                4,5,6},
+
+        A_resutl= { 1,4
+                    2,5
+                    3,6}
+
+    */
+
+    int aux = 0, aux2 = 0, aux3 = 0, aux4 = 0;
+    cout << "Indique cuantas filas tendra su matriz: ";cin >> aux;cin.ignore(256, '\n');
+    cout << "Indique cuantas columnas tendra sus matriz: ";cin >> aux2;cin.ignore(256, '\n');
+    int A[aux][aux2];
+
+    // Ejemplo rapido
+    // aux = 2; aux2=3;
+    // int   A[aux][aux2] = {   1,2,3,        
+    //                         4,5,6};
+    int A_resutl[aux2][aux];
+
+    // Proceso normal
     cout << endl;
     cout << endl;
     for (int x = 0; x < aux; x++)
     {
-        for (int y = 0; y < aux; y++)
-        {
-            C[x][y]=0;
-            for (int i = 0; i < aux; i++)
-            {
-                C[x][y]=C[x][y]+     (A[x][i]*B[i][y]);
-            }
-            cout<<C[x][y]<<" ";
-        }
+        for (int y = 0; y < aux2; y++)
+        {cout << "Ingrese el valor para la matriz A, en la posicion [" << x << "][" << y << "]: ";cin >> A[x][y];cin.ignore(256, '\n');}cout << endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int x = 0; x < aux; x++)
+    {
+        for (int y = 0; y < aux2; y++)
+        {cout<<A[x][y]<<" "; }
         cout << endl;
     }
-
+    // 
+    for (int x = 0; x < aux2; x++)
+    {
+        for (int y = 0; y < aux; y++)
+        A_resutl[x][y] = A[y][x];
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int x = 0; x < aux2; x++)
+    {
+        for (int y = 0; y < aux; y++)
+        {cout<<A_resutl[x][y]<<" "; }
+        cout << endl;
+    }
 
 
     return 0;
